e3fe1c05239e5fa8cd10319d6fc57187a173001b
Igor Rodionov <goruha@gmail.com>
2013-09-30 14:24:45 +0700

    add cron job

--- chef/environments/env_whitebox/recipes/default.rb
+++ chef/environments/env_whitebox/recipes/default.rb
@@ -1,3 +1,13 @@
 include_recipe "role_base"
 include_recipe "role_db"
 include_recipe "role_webserver"
+template '/etc/cron.d/chef_solo' do
+  source 'chef_solo.cron.erb'
+  owner 'root'
+  group 'root'
+  variables({
+    :chef_path => '/var/chef'
+  })
+  mode 00644
+end
+
--- solo.rb
+++ solo.rb
@@ -1,2 +1,2 @@
-cookbook_path ["cookbooks", "chef/applications", "chef/environments", "chef/roles"]
-json_attribs node.json"
+cookbook_path [File.expand.path(cookbooks", __FILE__), File.expand.path("..chef/applications", __FILE__), File.expand.path("..chef/environments", __FILE__), File.expand.path("..chef/roles", __FILE__)]
+json_attribs File.expand.path("node.json", __FILE__)
