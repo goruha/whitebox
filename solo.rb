ac0884554578e263f050bf31fa6700330789adf7
Igor Rodionov <goruha@gmail.com>
2013-09-30 14:26:37 +0700

    add cron job

--- solo.rb
+++ solo.rb
@@ -1,2 +1,2 @@
-cookbook_path [File.expand.path("../cookbooks", __FILE__), File.expand.path("..chef/applications", __FILE__), File.expand.path("..chef/environments", __FILE__), File.expand.path("..chef/roles", __FILE__)]
+cookbook_path [File.expand.path("../cookbooks", __FILE__), File.expand.path("../chef/applications", __FILE__), File.expand.path("../chef/environments", __FILE__), File.expand.path("../chef/roles", __FILE__)]
 json_attribs File.expand.path("../node.json", __FILE__)
