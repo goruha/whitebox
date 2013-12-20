c6355f95297de8f65def8811365b2369aa740d89
vovkats <vladimirts1991@gmail.com>
2013-12-20 14:42:09 +0700

    change configs

---
+++ chef/environments/env_development/attributes/default.rb
@@ -0,0 +1,5 @@
+set['mysql']['server_root_password'] = 'root'
+set['mysql']['server_debian_password'] = 'root'
+set['mysql']['server_repl_password'] = 'root'
+set['mysql']['tunable']['innodb_file_per_table'] = 1
+set['mysql']['tunable']['max_allowed_packet'] = '256M'
