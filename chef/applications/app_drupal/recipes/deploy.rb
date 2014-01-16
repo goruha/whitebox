a658150fe56a984b45d513c4b1574cf17468c54c
vovkats <vladimirts1991@gmail.com>
2014-01-16 17:07:51 +0700

    added capistrano-rvm

--- chef/applications/app_drupal/recipes/deploy.rb
+++ chef/applications/app_drupal/recipes/deploy.rb
@@ -1,6 +1,11 @@
 include_recipe "rvm::system"
 include_recipe "rvm::user_install"
-gem_package "capistrano" do
+#gem_package "capistrano" do
+#  action :install
+#  version '2.15.5'
+#end
+
+gem_package "capistrano-rvm" do
   action :install
-  version '2.15.5'
+  version '3.0'
 end
