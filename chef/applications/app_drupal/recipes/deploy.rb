241671819af802352dd8321c2c3016bc82501786
VladimirTS <VladimirTS1991@gmail.com>
2014-01-16 17:19:09 +0700

    Update deploy.rb

--- chef/applications/app_drupal/recipes/deploy.rb
+++ chef/applications/app_drupal/recipes/deploy.rb
@@ -1,11 +1,10 @@
 include_recipe "rvm::system"
 include_recipe "rvm::user_install"
-#gem_package "capistrano" do
-#  action :install
-#  version '2.15.5'
-#end
+gem_package "capistrano" do
+  action :install
+  version '2.15.5'
+end
 
 gem_package "capistrano-rvm" do
   action :install
-  version '3.0.0'
 end
