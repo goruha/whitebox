313982cb010d0c2d6a4cd879ab136e78d2e9ee1d
vovkats <vladimirts1991@gmail.com>
2014-01-17 11:43:52 +0700

    added user for rvm

--- chef/applications/app_drupal/recipes/deploy.rb
+++ chef/applications/app_drupal/recipes/deploy.rb
@@ -1,5 +1,10 @@
 include_recipe "rvm::system"
 include_recipe "rvm::user_install"
+
+node['rvm']['user_installs'] = [
+    { 'user' => 'jenkins' }
+]
+
 gem_package "capistrano" do
   action :install
   version '2.15.5'
