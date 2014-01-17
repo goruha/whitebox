ea9771abbb070a725fe0bbe22fa706182280171c
vovkats <vladimirts1991@gmail.com>
2014-01-17 12:01:02 +0700

    install rvm for jenkins

--- chef/applications/app_drupal/recipes/deploy.rb
+++ chef/applications/app_drupal/recipes/deploy.rb
@@ -2,8 +2,15 @@ include_recipe "rvm::system"
 include_recipe "rvm::user_install"
 
 node['rvm']['user_installs'] = [
-    { 'user' => 'jenkins' }
+    { :user => 'jenkins' }
 ]
+
+execute 'install_rvm' do
+  user('jenkins')
+  user = "jenkins"
+  command "su -l #{user} -c '\curl https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable'"
+  not_if { ::File.exists? "/home/#{user}/.rvm" }
+end
 
 gem_package "capistrano" do
   action :install
