52211160844a9065f6eeb37555e10e2d3deb1ae4
vovkats <vladimirts1991@gmail.com>
2014-01-17 17:57:56 +0700

    symbolic link

--- chef/roles/role_ci/recipes/default.rb
+++ chef/roles/role_ci/recipes/default.rb
@@ -25,9 +25,16 @@ cookbook_file "#{ssh_dir}/id_rsa" do
   action :create
 end
 
+
+
 include_recipe "app_jenkins"
 include_recipe "jenkins::proxy"
 include_recipe "app_drupal::develop"
 include_recipe "app_drupal::deploy"
 #include_recipe "drush"
+
+
+link "/home/jenkins/.rvm/bin/rvm" do
+  to "/usr/bin"
+end
 
